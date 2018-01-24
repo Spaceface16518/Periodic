2d79394037ca42bd4358f503b02641284a55aae3
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-23 21:27:02 -0600

    Added MacOS checker

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
+let isMacos; // Some MacOS operations are different from others
+if (process.platform === 'darwin') {
+  isMacos = true;
+} else {
+  isMacos = false;
+}
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
