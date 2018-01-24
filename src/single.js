1ced9da4ec507e8807eae6d932972a1f43dbce59
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-23 21:26:31 -0600

    added database open and close
    also known as a db skeleton

---
+++ src/single.js
@@ -0,0 +1,18 @@
+/* jshint esversion: 6  */
+const sqlite = require('sqlite3');
+
+let db = new sqlite.Database('../assets/data/periodic.db', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Connected to the Periodic Database.");
+  }
+});
+
+db.close((err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Disconnected from the Periodic Database.");
+  }
+});
