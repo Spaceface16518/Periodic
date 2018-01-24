957dcbfba6662dd94378c4a6542fb59b83c93fca
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-23 21:30:53 -0600

    on ready function

--- main.js
+++ main.js
@@ -44,7 +44,11 @@ mainWindow.webContents.openDevTools();
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function () {
+  createWindow();
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
