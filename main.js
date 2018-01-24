1fbc4b5d20385c09af4ae0524ce442681a458530
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-23 21:29:50 -0600

    Added semicolons
    Because jshint is a butt

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ if (process.platform === 'darwin') {
 }
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
 function createWindow() {
   // Create the browser window.
@@ -26,7 +26,7 @@ function createWindow() {
     pathname: path.join(__dirname, './src/index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }));
 
   // Open the DevTools.
 mainWindow.webContents.openDevTools();
@@ -36,8 +36,8 @@ mainWindow.webContents.openDevTools();
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
@@ -52,15 +52,15 @@ app.on('window-all-closed', function () {
   if (isMacos === false) {
     app.quit();
   }
-})
+});
 
 app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
