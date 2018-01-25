453a61bf82f28903c80005d3fc3712865c646589
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-24 23:08:01 -0600

    Added function to open the element

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@
 const Electron = require('electron');
 const path = require('path');
 const BrowserWindow = Electron.remote.BrowserWindow;
+
+function expandElement(elementId) {
+  let assetPath = path.join(__dirname + '/single.html');
+  let win = new BrowserWindow({
+    width: 200,
+    height: 275
+  });
+  win.on('close', () => {
+    win = null;
+  });
+  win.loadURL(assetPath);
+  win.show();
+}
+// TODO: add function that calls this function
