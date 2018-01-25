8825d870bf12bf20006d1f865931f41ca1231425
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-24 23:09:19 -0600

    onclick function

--- src/single.js
+++ src/single.js
@@ -3,5 +3,11 @@ import Element from '../assets/modules/Element';
 const sqlite = require('sqlite');
 let db = new sqlite.Database('../assets/data/Periodic_Table.db');
 
+let id;
+
+$("td").click(function() {
+  id = this.id;
+  let Elem = new Element(id);
+  write();
 });
 
