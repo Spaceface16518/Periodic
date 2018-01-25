8ecbc86137df81c7cd54037c6c07b6080dac8435
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-24 23:08:59 -0600

    Got rid of sql stuff

--- src/single.js
+++ src/single.js
@@ -3,18 +3,5 @@ import Element from '../assets/modules/Element';
 const sqlite = require('sqlite');
 let db = new sqlite.Database('../assets/data/Periodic_Table.db');
 
-let db = new sqlite.Database('../assets/data/periodic.db', (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log("Connected to the Periodic Database.");
-  }
 });
 
-db.close((err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log("Disconnected from the Periodic Database.");
-  }
-});
