cf2ba2ca53ce5f9d2d8a1d29c4b2e3d57d1c6eee
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-25 22:28:42 -0600

    Routine maintenance
    - deleted unneccessary file
    - fixed sourcing error in index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const BrowserWindow = Electron.remote.BrowserWindow;
 
 function expandElement(elementId) {
-  let assetPath = path.join(__dirname + '/single.html');
+  let assetPath = path.join(__dirname + './single.html');
   let win = new BrowserWindow({
     width: 200,
     height: 275
