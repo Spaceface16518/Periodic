e23b247ad8caf2eb4eeb7022322dc7a40e60ab3b
Spaceface16518 <33424649+Spaceface16518@users.noreply.github.com>
2018-01-27 22:44:35 -0600

    Automate filling of HTML elements array

--- src/single.js
+++ src/single.js
@@ -10,17 +10,34 @@ const qb = new sqlite.Database('../assets/data/Query.db');
 // The single.html page will have areas with id's that will be filled
 // by this function. Please keep the naming convention of the id's with
 // the naming of the tables in the database.
+let query = () => {
+  qb.get('SELECT query FROM Queries WHERE queryID=$maxid', {
+    $maxid: getMaxId();
+  })
+}
+
 write = () => {
-  let HTMLelements = [/* None currently available */]; // Fill with elements available in single.html
-  $.each(HTMLelements, function (i) {
-    $(HTMLelements[i]).click(function () {
+  let HTMLelements = []; // Fill with elements available in single.html
+  fillHTMLelements();
+  $.each(HTMLelements, function(i) {
+    $(HTMLelements[i]).click(function() {
       let selector = "#" + HTMLelements[i];
       $(selector).text(Elem.getInfo(HTMLelements[i]));
     });
   });
-};getMaxId = () => {
+};
+
+getMaxId = () => {
   qb.get('SELECT MAX(queryID) FROM Queries', (err, rows) => {
     logErr(err);
     return rows
   })
+}
+
+fillHTMLelements = () => {
+  // NOTE: The HTMLDOM method getElementByClassName returns an array
+  let elements = document.getElementsByClassName('element');
+  for (var i = 0; i < elements.length; i++) {
+    HTMLelements.push(elements[i])
+  }
 }
